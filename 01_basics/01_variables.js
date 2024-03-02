75e59ea75d2d33530221ce47debd9581e347ed5b
Swati Agarwal <69628293+swati57@users.noreply.github.com>
2024-03-02 16:12:26 +0000

    Variables file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const id = 123;
+let email = "swati@gmail.com"
+//use const and let only
+//prefer not to use var bcoz of issue in its block scope and function
+var pass="123"
+city="kolkata" //this is also possible but not preferred
+let state //undefined
+
+email = "swati1@gmail.com"
+pass="1233"
+city="kolkataa"
+
+console.table([id,email,pass,city,state])
