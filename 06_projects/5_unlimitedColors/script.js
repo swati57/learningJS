02f572d6c5373db7d20dda0406ca7a89bbea1271
Swati Agarwal <69628293+swati57@users.noreply.github.com>
2024-03-15 11:52:00 +0000

    Hands on Events

---
+++ 06_projects/5_unlimitedColors/script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body")
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i=0;i<6;++i){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+
+console.log(randomColor());
+let intervalId
+const startChangingColor = function(){
+    if(!intervalId)
+    intervalId=setInterval(change,1000);
+
+    function change(){
+    document.body.style.backgroundColor = randomColor();
+    }
+}
+const start = document.querySelector('#start').
+addEventListener('click', startChangingColor )
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+const stop = document.querySelector('#stop').
+addEventListener('click', stopChangingColor )
